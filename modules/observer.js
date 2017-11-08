a55f70c0e9055b7a3341facedb3b91db31ace76f
BIT Student <classroom@bgit.rs>
2017-11-08 16:07:00 +0100

    moduls

---
+++ modules/observer.js
@@ -0,0 +1,11 @@
+class Observer{
+    constructor(name){
+        this.name = name;
+    }
+  
+    notify(){
+        console.log(`${this.name} :memory overload.`);
+    }
+}
+
+module.exports = Observer;
